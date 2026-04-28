d538adbaa1dde6575be68915795ba12938143608
Eliyabujilima <emasbujilima3@gmail.com>
2026-04-28 17:27:20 +0300

    Fix MYSQLPORT environment variable handling

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT")),
+        port=int(os.getenv("MYSQLPORT", "3306")),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE")
